cb38d5d7de096158b522c64c342e09e7d294d57f
Pierre-Loic FELTER <plfelter@argans.co.uk>
2023-02-02 11:09:14 +0100

    added the inifinite loop that will poll the directory for new files and use the copy function every 2 secs

--- cplog.sh
+++ cplog.sh
@@ -1,8 +1,19 @@
 log_file="log.txt"
-cp_result=$(cp -rnv ./cptest ./cptest4)
-if [ ! -z "$cp_result" ]; then
-        echo "$cp_result"
-        echo >> $log_file
-        date >> $log_file
-        echo "$cp_result" >> $log_file
-fi
+source_dir="./cptest"
+dest_dir="./cptest4"
+
+function copy_and_log {
+        cp_result=$(cp -rnv $source_dir $dest_dir)
+        if [ ! -z "$cp_result" ]; then
+                echo "$cp_result"
+                echo >> $log_file
+                date >> $log_file
+                echo "$cp_result" >> $log_file
+        fi
+}
+
+while :
+do
+        copy_and_log
+        sleep 2
+done
