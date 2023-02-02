b3d4f20564a728969c31eaafd7964830492e2574
pilvdl <123661919+pilvdl@users.noreply.github.com>
2023-02-02 11:52:05 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-script_dir=$(dirname -- "$( readlink -f -- "$0"; )";)
-cd $script_dir
-python3 main.py
+#script_dir=$(dirname -- "$( readlink -f -- "$0"; )";)
+#cd $script_dir
+python3 /home/debian/lbc_immo/main.py
